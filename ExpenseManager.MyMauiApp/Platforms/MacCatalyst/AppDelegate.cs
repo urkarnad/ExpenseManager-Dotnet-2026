5635ec1896a0041965b7defb6b778bbcb16a87c0
Theodora <d.hrechko@ukma.edu.ua>
2026-03-13 15:10:59 +0200

    Delete ExpenseManager.MyMauiApp directory

--- ExpenseManager.MyMauiApp/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace ExpenseManager.MyMauiApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
