2f7f883f07b4e08b64a2da6fae1fb08253d854a8
Theodora Hrechko <d.hrechko@ukma.edu.ua>
2026-03-13 14:30:17 +0200

    maui tesr ver

--- ExpenswsManager.MyMauiApp/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using ExpenswsManager.MyMauiApp.Data;
-global using ExpenswsManager.MyMauiApp.PageModels;
-global using ExpenswsManager.MyMauiApp.Pages;
-global using ExpenswsManager.MyMauiApp.Services;
-global using ExpenswsManager.MyMauiApp.Utilities;
-global using Fonts;
--- ExpenswsManager.MyMauiApp/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ExpenswsManager.MyMauiApp")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ExpenswsManager.MyMauiApp.Pages")]
