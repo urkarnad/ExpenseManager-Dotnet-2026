25da4977cb0fa5991e467244472e4467b681d54b
Theodora Hrechko <d.hrechko@ukma.edu.ua>
2026-03-13 15:41:29 +0200

    maui app

--- MyMauiApp/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Fonts;
-global using MyMauiApp.Data;
-global using MyMauiApp.PageModels;
-global using MyMauiApp.Pages;
-global using MyMauiApp.Services;
-global using MyMauiApp.Utilities;
